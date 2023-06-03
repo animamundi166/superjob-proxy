0628b738c756469a28416f097c23e7f72a5bb466
animamundi166 <animamundi166@gmail.com>
2023-06-03 14:33:15 +0300

    add proxy

---
+++ index.js
@@ -0,0 +1,66 @@
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+import proxy from 'express-http-proxy';
+import requestIp from 'request-ip';
+import { LRUCache } from 'lru-cache'
+
+const app = express();
+const port = process.env.PORT;
+const cookiePerIP = new LRUCache({
+  max: 1000,
+  // how long to live in ms
+  ttl: 1000 * 60 * 3, // 3 min
+  updateAgeOnGet: false,
+  updateAgeOnHas: false,
+});
+
+app.use(cors({
+  origin: true,
+}));
+
+app.use(requestIp.mw());
+
+app.use('/', proxy(process.env.ORIGINAL_SERVER_URL, {
+  https: true,
+  userResHeaderDecorator(headers, userReq, userRes, proxyReq, proxyRes) {
+    const key = userReq.clientIp;
+
+    if (headers['set-cookie']) {
+      const newCookies = headers['set-cookie'].map(c => {
+        const [key, value] = c.split(';')[0].split('=');
+        return { key, value };
+      });
+
+      const previousCookies = cookiePerIP.get(key) || [];
+      const currentCookies = previousCookies.concat(newCookies);
+
+      cookiePerIP.set(key, currentCookies);
+    }
+
+    return headers;
+  },
+  proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
+    const key = srcReq.clientIp;
+
+    if (cookiePerIP.has(key)) {
+      const cookies = cookiePerIP
+        .get(key)
+        .map(c => `${c.key}=${c.value}`)
+        .join(';')
+
+      proxyReqOpts.headers['cookie'] = cookies;
+    }
+
+    return proxyReqOpts;
+  },
+}));
+
+app.use((err, _req, res, _next) => {
+  console.error(err.stack);
+  res.status(500).send('Server error');
+});
+
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+});
